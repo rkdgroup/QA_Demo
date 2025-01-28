a47428e74ce54e984ace20bca994aec0260c3e02
KamalaEas <156311408+KamalaEas@users.noreply.github.com>
2025-01-28 22:02:07 +0530

    new project

---
+++ src/main/java/sampleJenkins.java
@@ -0,0 +1,68 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class sampleJenkins {
+    public static void main(String[] args) {
+        WebDriver driver;
+        String FS = File.separator;
+        String downloadFilepath = System.getProperty("user.dir") + FS + "src" + FS + "test" + FS + "resources" + FS + "Downloads";
+
+        System.out.println("Hello Jenkins");
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.prompt_for_download", "false");
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        ChromeOptions chromeOptions = getChromeOptions(downloadFilepath);
+        System.setProperty("webdriver.chrome.silentOutput","true");
+
+        chromeOptions.setExperimentalOption("prefs", chromePrefs);
+        chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        chromeOptions.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR,"accept");
+        chromeOptions.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+
+
+    }
+
+    private static ChromeOptions getChromeOptions(String downloadFilepath) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("window-size=1366,768"); // New Added
+        chromeOptions.setBinary(downloadFilepath);
+
+
+        chromeOptions.addArguments("--disable-extensions"); // disabling extensions
+        chromeOptions.addArguments("--disable-gpu"); // applicable to windows os only
+        chromeOptions.addArguments("--disable-impl-side-painting");
+        chromeOptions.addArguments("--remote-debugging-port=9222");
+        chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        chromeOptions.addArguments("--no-sandbox"); // Bypass OS security model
+        chromeOptions.addArguments("enable-automation");
+        chromeOptions.addArguments("--dns-prefetch-disable");
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NONE);
+        // Generate a unique temporary user data directory
+        try {
+            Path tempDir = Files.createTempDirectory("chrome-user-data");
+            chromeOptions.addArguments("--user-data-dir=" + tempDir.toString());
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the error if directory creation fails
+        }
+        return chromeOptions;
+    }
+}
