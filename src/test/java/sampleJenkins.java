69ca548e4d5567b5a0daac44111872d45368ab85
KamalaEas <156311408+KamalaEas@users.noreply.github.com>
2025-01-28 22:29:35 +0530

    Update sampleJenkins.java

--- src/test/java/sampleJenkins.java
+++ src/test/java/sampleJenkins.java
@@ -33,7 +33,7 @@ public class sampleJenkins {
         driver = new ChromeDriver(chromeOptions);
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();*/
-        System.setProperty("webdriver.chrome.driver","D:\\IdeaProjects\\QA_Demo\\src\\test\\resources\\downloads\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","D:\\IdeaProjects\\QA_Demo\\src\\test\\resources\\downloads\\chromedriver");
         driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
